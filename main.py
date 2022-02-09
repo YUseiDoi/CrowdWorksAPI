6dcc5a595054a57890e030d27580e08610e20456
YUseiDoi <yusedo1101@gmail.com>
2022-02-10 00:04:28 +0900

    20220210 add options which get only new projects

--- main.py
+++ main.py
@@ -1,121 +1,50 @@
 from flask import Flask, render_template
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from enum import Enum
-import datetime
+from standard_function import InitCrowdworks, SearchKeyword, GetALlAnkenName, GetNewProjects
 
 app = Flask(__name__)
 
-# Type of WebElement
-class ElementType(Enum):
-    ID = 0
-    Class = 1
-    Name = 2
-    Tag = 3
-    PartialLinkText = 4
-    Label = 5
-    Linktext = 6
-    Xpath = 7
-    Unknown = -1
-
-@app.route("/allvbaprojects/")
+@app.route("/vba/all/")
 def CW_AllVBAProjects():    # get all of VBA projects
     driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    GetWebElement(driver, ElementType.Class, "search_freeword", 10).find_element_by_tag_name("input").send_keys("VBA")      # 検索テキストボックスを取得、VBAと入力
-    GetWebElement(driver, ElementType.Class, "cw-input_group_button", 10).find_element_by_tag_name("button").click()  # 検索ボタンを押す
-    eachAnkenName = GetALlAnkenName(driver)
+    SearchKeyword(driver, "VBA")        # keyword search
+    eachAnkenName = GetALlAnkenName(driver)         # get all of project's name
+    driver.close()
     return render_template('index.html', data=zip(eachAnkenName))
 
-@app.route("/allpythonprojects/")
+@app.route("/python/all/")
 def CW_AllPythonProjects():     # get all of Python projects
     driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    GetWebElement(driver, ElementType.Class, "search_freeword", 10).find_element_by_tag_name("input").send_keys("Python")      # 検索テキストボックスを取得、VBAと入力
-    GetWebElement(driver, ElementType.Class, "cw-input_group_button", 10).find_element_by_tag_name("button").click()  # 検索ボタンを押す
-    eachAnkenName = GetALlAnkenName(driver)
+    SearchKeyword(driver, "Python")        # keyword search
+    eachAnkenName = GetALlAnkenName(driver)         # get all of project's name
+    driver.close()
     return render_template('index.html', data=zip(eachAnkenName))
 
-@app.route("/allapiprojects/")
+@app.route("/api/all/")
 def CW_AllAPIProjects():        # get all of API projects
     driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    GetWebElement(driver, ElementType.Class, "search_freeword", 10).find_element_by_tag_name("input").send_keys("API")      # 検索テキストボックスを取得、VBAと入力
-    GetWebElement(driver, ElementType.Class, "cw-input_group_button", 10).find_element_by_tag_name("button").click()  # 検索ボタンを押す
-    eachAnkenName = GetALlAnkenName(driver)
+    SearchKeyword(driver, "API")        # keyword search
+    eachAnkenName = GetALlAnkenName(driver)         # get all of project's name
+    driver.close()
     return render_template('index.html', data=zip(eachAnkenName))
 
-# Init Chrome instance and go to CrowdWorks
-def InitCrowdworks():
-    driver = webdriver.Chrome()
-    driver.get("https://crowdworks.jp/public/jobs?category=jobs&order=score&ref=toppage_hedder")
-    return driver
+@app.route("/vba/new/")
+def CW_NewVBAProjects():        # get new vba projects
+    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
+    newProjects = GetNewProjects(driver, "VBA")     # get new projects
+    driver.close()
+    return render_template('index.html', data=zip(newProjects))
 
-# get WebElement
-def GetWebElement(driver, elementType, elementName, timeout):
-    now = InitTime()    # Init time instance
-    nowTime = int(now.strftime('%Y%m%d%H%M%S'))      # get now time
-    untilTime = nowTime + timeout       # calc timeout
-    # loop until nowtime gets larger than timeout
-    while nowTime < untilTime:
-        if elementType == ElementType.ID:
-            webElement = driver.find_element_by_id(elementName)
-        elif elementType == ElementType.Class:
-            webElement = driver.find_element_by_class_name(elementName)
-        elif elementType == ElementType.Name:
-            webElement = driver.find_element_by_name(elementName)
-        elif elementType == ElementType.Tag:
-            webElement = driver.find_element_by_tag_name(elementName)
-        elif elementType == ElementType.PartialLinkText:
-            webElement = driver.find_element_by_partial_link_text(elementName)
-        elif elementType == ElementType.Xpath:
-            webElement = driver.find_element_by_xpath(elementName)
-        elif elementType == ElementType.Linktext:
-            webElement = driver.find_element_by_link_text(elementName)
-        else:
-            print("予期せぬエラーが発生しました")
-        if webElement != None:
-            break
-        nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
-    return webElement
+@app.route("/python/new/")
+def CW_NewPythonProjects():        # get new vba projects
+    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
+    newProjects = GetNewProjects(driver, "Python")     # get new projects
+    driver.close()
+    return render_template('index.html', data=zip(newProjects))
 
-# get WebElements
-def GetWebElements(driver, elementType, elementName, timeout):
-    now = InitTime()    # Init time instance
-    nowTime = int(now.strftime('%Y%m%d%H%M%S'))      # get now time
-    untilTime = nowTime + timeout       # calc timeout
-    # loop until nowtime gets larger than timeout
-    while nowTime < untilTime:
-        if elementType == ElementType.ID:
-            webElements = driver.find_elements_by_id(elementName)
-        elif elementType == ElementType.Class:
-            webElements = driver.find_elements_by_class_name(elementName)
-        elif elementType == ElementType.Name:
-            webElements = driver.find_elements_by_name(elementName)
-        elif elementType == ElementType.Tag:
-            webElements = driver.find_elements_by_tag_name(elementName)
-        elif elementType == ElementType.PartialLinkText:
-            webElements = driver.find_elements_by_partial_link_text(elementName)
-        elif elementType == ElementType.Xpath:
-            webElements = driver.find_elements_by_xpath(elementName)
-        elif elementType == ElementType.Linktext:
-            webElements = driver.find_elements_by_link_text(elementName)
-        else:
-            print("予期せぬエラーが発生しました")
-        if webElements != None:
-            break
-        nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
-    return webElements
+@app.route("/api/new/")
+def CW_NewAPIProjects():        # get new vba projects
+    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
+    newProjects = GetNewProjects(driver, "API")     # get new projects
+    driver.close()
+    return render_template('index.html', data=zip(newProjects))
 
-# Init time instance
-def InitTime():
-    t_delta = datetime.timedelta(hours=9)
-    JST = datetime.timezone(t_delta, 'JST')
-    now = datetime.datetime.now(JST)
-    return now
-
-# get all of project's name
-def GetALlAnkenName(driver):
-    webElements = GetWebElements(driver, ElementType.Class, "item_title", 10)
-    eachAnkenName = []
-    for eachElement in webElements:
-        eachAnkenName.append(eachElement.text)
-    return eachAnkenName
---
+++ standard_function.py
@@ -0,0 +1,120 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from enum import Enum
+import datetime
+from selenium.webdriver.support.select import Select
+
+# Type of WebElement
+class ElementType(Enum):
+    ID = 0
+    Class = 1
+    Name = 2
+    Tag = 3
+    PartialLinkText = 4
+    Label = 5
+    Linktext = 6
+    Xpath = 7
+    Unknown = -1
+
+# Init Chrome instance and go to CrowdWorks
+def InitCrowdworks():
+    option = Options()
+    option.add_argument('--headless')
+    driver = webdriver.Chrome(options=option)
+    driver.get("https://crowdworks.jp/public/jobs?category=jobs&order=score&ref=toppage_hedder")
+    return driver
+
+# get WebElement
+def GetWebElement(driver, elementType, elementName, timeout):
+    now = InitTime()    # Init time instance
+    nowTime = int(now.strftime('%Y%m%d%H%M%S'))      # get now time
+    untilTime = nowTime + timeout       # calc timeout
+    # loop until nowtime gets larger than timeout
+    while nowTime < untilTime:
+        if elementType == ElementType.ID:
+            webElement = driver.find_element_by_id(elementName)
+        elif elementType == ElementType.Class:
+            webElement = driver.find_element_by_class_name(elementName)
+        elif elementType == ElementType.Name:
+            webElement = driver.find_element_by_name(elementName)
+        elif elementType == ElementType.Tag:
+            webElement = driver.find_element_by_tag_name(elementName)
+        elif elementType == ElementType.PartialLinkText:
+            webElement = driver.find_element_by_partial_link_text(elementName)
+        elif elementType == ElementType.Xpath:
+            webElement = driver.find_element_by_xpath(elementName)
+        elif elementType == ElementType.Linktext:
+            webElement = driver.find_element_by_link_text(elementName)
+        else:
+            print("予期せぬエラーが発生しました")
+        if webElement != None:
+            break
+        nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
+    return webElement
+
+# get WebElements
+def GetWebElements(driver, elementType, elementName, timeout):
+    now = InitTime()    # Init time instance
+    nowTime = int(now.strftime('%Y%m%d%H%M%S'))      # get now time
+    untilTime = nowTime + timeout       # calc timeout
+    # loop until nowtime gets larger than timeout
+    while nowTime < untilTime:
+        if elementType == ElementType.ID:
+            webElements = driver.find_elements_by_id(elementName)
+        elif elementType == ElementType.Class:
+            webElements = driver.find_elements_by_class_name(elementName)
+        elif elementType == ElementType.Name:
+            webElements = driver.find_elements_by_name(elementName)
+        elif elementType == ElementType.Tag:
+            webElements = driver.find_elements_by_tag_name(elementName)
+        elif elementType == ElementType.PartialLinkText:
+            webElements = driver.find_elements_by_partial_link_text(elementName)
+        elif elementType == ElementType.Xpath:
+            webElements = driver.find_elements_by_xpath(elementName)
+        elif elementType == ElementType.Linktext:
+            webElements = driver.find_elements_by_link_text(elementName)
+        else:
+            print("予期せぬエラーが発生しました")
+        if webElements != None:
+            break
+        nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
+    return webElements
+
+# Init time instance
+def InitTime():
+    t_delta = datetime.timedelta(hours=9)
+    JST = datetime.timezone(t_delta, 'JST')
+    now = datetime.datetime.now(JST)
+    return now
+
+# get all of project's name
+def GetALlAnkenName(driver):
+    webElements = GetWebElements(driver, ElementType.Class, "item_title", 10)
+    eachAnkenName = []
+    for eachElement in webElements:
+        eachAnkenName.append(eachElement.text)
+    return eachAnkenName
+
+# keyword search
+def SearchKeyword(driver, keyword):
+    GetWebElement(driver, ElementType.Class, "search_freeword", 10).find_element_by_tag_name("input").send_keys(keyword)      # 検索テキストボックスを取得、VBAと入力
+    GetWebElement(driver, ElementType.Class, "cw-input_group_button", 10).find_element_by_tag_name("button").click()  # 検索ボタンを押す
+
+# get new projects
+def GetNewProjects(driver, keyword):
+    SearchKeyword(driver, keyword)
+    Select(GetWebElement(driver, ElementType.Class, "cw-pull_left", 10).find_element_by_tag_name("select")).select_by_index(1)      # select 新着
+    webElement = GetWebElement(driver, ElementType.Class, "search_results", 10)
+    webElements = webElement.find_elements_by_class_name("new")
+    NewProjects = []
+    for eachElement in webElements:     # only append new projects
+        NewProjects.append(eachElement.text)
+    return NewProjects
+
+# check if there is "新着" class
+def CheckNew(webElement):
+    if len(webElement.find_elements_by_class_name("ribbon_job_offer_options")) > 0:
+        return True
+    else:
+        return False
