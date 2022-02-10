ae8d7d75c27b2b9e9084e5df0df8cad0cc0ea9f5
YUseiDoi <yusedo1101@gmail.com>
2022-02-10 23:17:25 +0900

    20220210 create two classes

--- main.py
+++ main.py
@@ -1,60 +1,83 @@
 from flask import Flask, render_template, request
-from standard_function import InitCrowdworks, SearchKeyword, GetALlAnkenName, GetNewProjects, ConvertJSON, CreateDict, GetAllAnkenURLs, CreateAnkenID, GetInfoAndCreateJSON
+from standard_function import ChromeOperation, DataOperation
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False         # change character code
 
 @app.route("/vba/all/")
-def CW_AllVBAProjects():    # get all of VBA projects
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, "VBA")        # keyword search
-    jsonData = GetInfoAndCreateJSON(driver)            # get information and create json data
+def CW_AllVBAProjects():            # get all of VBA projects
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword("VBA")              # keyword search
+    ALlAnkenID, ALlAnkenName, AllAnkenURL = myChromeOperation.GetAllInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(ALlAnkenID, ALlAnkenName, AllAnkenURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
     return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
 @app.route("/python/all/")
 def CW_AllPythonProjects():     # get all of Python projects
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, "Python")        # keyword search
-    jsonData = GetInfoAndCreateJSON(driver)            # get information and create json data
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword("Python")              # keyword search
+    ALlAnkenID, ALlAnkenName, AllAnkenURL = myChromeOperation.GetAllInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(ALlAnkenID, ALlAnkenName, AllAnkenURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
     return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
 @app.route("/api/all/")
 def CW_AllAPIProjects():        # get all of API projects
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, "API")        # keyword search
-    jsonData = GetInfoAndCreateJSON(driver)            # get information and create json data
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword("API")              # keyword search
+    ALlAnkenID, ALlAnkenName, AllAnkenURL = myChromeOperation.GetAllInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(ALlAnkenID, ALlAnkenName, AllAnkenURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
     return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
 @app.route("/vba/new/")
 def CW_NewVBAProjects():        # get new vba projects
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, "VBA")        # keyword search
-    newProjects = GetNewProjects(driver)     # get new projects
-    driver.close()
-    return render_template('index.html', data=zip(newProjects))
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword("VBA")              # keyword search
+    NewProjectsID, NewProjectsName, NewProjectsURL = myChromeOperation.GetNewProjectsInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(NewProjectsID, NewProjectsName, NewProjectsURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
+    return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
 @app.route("/python/new/")
 def CW_NewPythonProjects():        # get new vba projects
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, "Python")        # keyword search
-    newProjects = GetNewProjects(driver, "Python")     # get new projects
-    driver.close()
-    return render_template('index.html', data=zip(newProjects))
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword("Python")              # keyword search
+    NewProjectsID, NewProjectsName, NewProjectsURL = myChromeOperation.GetNewProjectsInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(NewProjectsID, NewProjectsName, NewProjectsURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
+    return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
 @app.route("/api/new/")
 def CW_NewAPIProjects():        # get new vba projects
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, "API")        # keyword search
-    newProjects = GetNewProjects(driver, "API")     # get new projects
-    driver.close()
-    return render_template('index.html', data=zip(newProjects))
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword("API")              # keyword search
+    NewProjectsID, NewProjectsName, NewProjectsURL = myChromeOperation.GetNewProjectsInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(NewProjectsID, NewProjectsName, NewProjectsURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
+    return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
-@app.route("/search/")
-def CW_KeywordSearch():
-    keyword = request.args.get("keyword")       # get keyword from parameter
-    driver = InitCrowdworks()       # Init Chrome instance and go to CrowdWorks
-    SearchKeyword(driver, keyword)        # keyword search
-    eachAnkenName = GetALlAnkenName(driver)          # get projects name
-    driver.close()
-    return render_template('index.html', data=zip(eachAnkenName))
+@app.route("/search/<keyword>/")
+def CW_KeywordSearch(keyword):
+    myChromeOperation = ChromeOperation()                   # create new instance of ChromeOperation
+    myDataOperation = DataOperation()                   # create new instance of DataOperation
+    myChromeOperation.SearchKeyword(keyword)              # keyword search
+    ALlAnkenID, ALlAnkenName, AllAnkenURL = myChromeOperation.GetAllInformation()                   # get information id, title, url
+    myChromeOperation.CloseChrome                   # close chrome instance
+    infoDict = myDataOperation.CreateDict(ALlAnkenID, ALlAnkenName, AllAnkenURL)        # create lists for JSON
+    jsonData = myDataOperation.ConvertJSON(infoDict)            # convert to JSON style
+    return render_template('index.html', status_code=("status_code: " + str(jsonData.status_code)), json_data=jsonData.json)
 
--- standard_function.py
+++ standard_function.py
@@ -17,140 +17,148 @@ class ElementType(Enum):
     Xpath = 7
     Unknown = -1
 
-# Init Chrome instance and go to CrowdWorks
-def InitCrowdworks():
-    option = Options()
-    option.add_argument('--headless')
-    driver = webdriver.Chrome(options=option)       # chrome is unvisible
-    #driver = webdriver.Chrome()         # chrome is visible
-    driver.get("https://crowdworks.jp/public/jobs?category=jobs&order=score&ref=toppage_hedder")
-    return driver
+# Selenium Class
+class ChromeOperation:
+    def __init__(self):
+        option = Options()
+        option.add_argument('--headless')
+        #self.driver = webdriver.Chrome(options=option)  # chrome is not visible
+        self.driver = webdriver.Chrome()         # chrome is visible
+        self.driver.get("https://crowdworks.jp/public/jobs?category=jobs&order=score&ref=toppage_hedder")
 
-# get WebElement
-def GetWebElement(driver, elementType, elementName, timeout):
-    now = InitTime()    # Init time instance
-    nowTime = int(now.strftime('%Y%m%d%H%M%S'))      # get now time
-    untilTime = nowTime + timeout       # calc timeout
-    # loop until nowtime gets larger than timeout
-    while nowTime < untilTime:
-        if elementType == ElementType.ID:
-            webElement = driver.find_element_by_id(elementName)
-        elif elementType == ElementType.Class:
-            webElement = driver.find_element_by_class_name(elementName)
-        elif elementType == ElementType.Name:
-            webElement = driver.find_element_by_name(elementName)
-        elif elementType == ElementType.Tag:
-            webElement = driver.find_element_by_tag_name(elementName)
-        elif elementType == ElementType.PartialLinkText:
-            webElement = driver.find_element_by_partial_link_text(elementName)
-        elif elementType == ElementType.Xpath:
-            webElement = driver.find_element_by_xpath(elementName)
-        elif elementType == ElementType.Linktext:
-            webElement = driver.find_element_by_link_text(elementName)
-        else:
-            print("予期せぬエラーが発生しました")
-        if webElement != None:
-            break
+    # get WebElement
+    def GetWebElement(self, elementType, elementName, timeout):
+        now = self.InitTime()  # Init time instance
         nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
-    return webElement
+        untilTime = nowTime + timeout  # calc timeout
+        # loop until nowtime gets larger than timeout
+        while nowTime < untilTime:
+            if elementType == ElementType.ID:
+                webElement = self.driver.find_element_by_id(elementName)
+            elif elementType == ElementType.Class:
+                webElement = self.driver.find_element_by_class_name(elementName)
+            elif elementType == ElementType.Name:
+                webElement = self.driver.find_element_by_name(elementName)
+            elif elementType == ElementType.Tag:
+                webElement = self.driver.find_element_by_tag_name(elementName)
+            elif elementType == ElementType.PartialLinkText:
+                webElement = self.driver.find_element_by_partial_link_text(elementName)
+            elif elementType == ElementType.Xpath:
+                webElement = self.driver.find_element_by_xpath(elementName)
+            elif elementType == ElementType.Linktext:
+                webElement = self.driver.find_element_by_link_text(elementName)
+            else:
+                print("予期せぬエラーが発生しました")
+            if webElement != None:
+                break
+            nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
+        return webElement
 
-# get WebElements
-def GetWebElements(driver, elementType, elementName, timeout):
-    now = InitTime()    # Init time instance
-    nowTime = int(now.strftime('%Y%m%d%H%M%S'))      # get now time
-    untilTime = nowTime + timeout       # calc timeout
-    # loop until nowtime gets larger than timeout
-    while nowTime < untilTime:
-        if elementType == ElementType.ID:
-            webElements = driver.find_elements_by_id(elementName)
-        elif elementType == ElementType.Class:
-            webElements = driver.find_elements_by_class_name(elementName)
-        elif elementType == ElementType.Name:
-            webElements = driver.find_elements_by_name(elementName)
-        elif elementType == ElementType.Tag:
-            webElements = driver.find_elements_by_tag_name(elementName)
-        elif elementType == ElementType.PartialLinkText:
-            webElements = driver.find_elements_by_partial_link_text(elementName)
-        elif elementType == ElementType.Xpath:
-            webElements = driver.find_elements_by_xpath(elementName)
-        elif elementType == ElementType.Linktext:
-            webElements = driver.find_elements_by_link_text(elementName)
-        else:
-            print("予期せぬエラーが発生しました")
-        if webElements != None:
-            break
+    # get WebElements
+    def GetWebElements(self, elementType, elementName, timeout):
+        now = self.InitTime()  # Init time instance
         nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
-    return webElements
+        untilTime = nowTime + timeout  # calc timeout
+        # loop until nowtime gets larger than timeout
+        while nowTime < untilTime:
+            if elementType == ElementType.ID:
+                webElements = self.driver.find_elements_by_id(elementName)
+            elif elementType == ElementType.Class:
+                webElements = self.driver.find_elements_by_class_name(elementName)
+            elif elementType == ElementType.Name:
+                webElements = self.driver.find_elements_by_name(elementName)
+            elif elementType == ElementType.Tag:
+                webElements = self.driver.find_elements_by_tag_name(elementName)
+            elif elementType == ElementType.PartialLinkText:
+                webElements = self.driver.find_elements_by_partial_link_text(elementName)
+            elif elementType == ElementType.Xpath:
+                webElements = self.driver.find_elements_by_xpath(elementName)
+            elif elementType == ElementType.Linktext:
+                webElements = self.driver.find_elements_by_link_text(elementName)
+            else:
+                print("予期せぬエラーが発生しました")
+            if webElements != None:
+                break
+            nowTime = int(now.strftime('%Y%m%d%H%M%S'))  # get now time
+        return webElements
 
-# Init time instance
-def InitTime():
-    t_delta = datetime.timedelta(hours=9)
-    JST = datetime.timezone(t_delta, 'JST')
-    now = datetime.datetime.now(JST)
-    return now
+    # Init time instance
+    def InitTime(self):
+        t_delta = datetime.timedelta(hours=9)
+        JST = datetime.timezone(t_delta, 'JST')
+        now = datetime.datetime.now(JST)
+        return now
 
-# get all of project's name
-def GetALlAnkenName(driver):
-    webElements = GetWebElements(driver, ElementType.Class, "item_title", 10)
-    eachAnkenName = []
-    for eachElement in webElements:
-        eachAnkenName.append(eachElement.text)
-    return eachAnkenName
+    # get all of project's name
+    def GetALlAnkenName(self):
+        webElements = self.GetWebElements(ElementType.Class, "item_title", 10)
+        eachAnkenName = []
+        for eachElement in webElements:
+            eachAnkenName.append(eachElement.text)
+        return eachAnkenName
 
-# get all project's URL
-def GetAllAnkenURLs(driver):
-    webElements = GetWebElements(driver, ElementType.Class, "item_title", 10)
-    AllAnkenURL = []
-    i = 0
-    for eachElement in webElements:
-        AllAnkenURL.append(webElements[i].find_element_by_tag_name("a").get_attribute("href"))
-    return AllAnkenURL
+    # get all project's URL
+    def GetAllAnkenURLs(self):
+        webElements = self.GetWebElements(ElementType.Class, "item_title", 10)
+        AllAnkenURL = []
+        i = 0
+        for i in range(len(webElements)):
+            AllAnkenURL.append(webElements[i].find_element_by_tag_name("a").get_attribute("href"))
+        return AllAnkenURL
 
+    # keyword search
+    def SearchKeyword(self, keyword):
+        self.GetWebElement(ElementType.Class, "search_freeword", 10).find_element_by_tag_name("input").send_keys(
+            keyword)  # 検索テキストボックスを取得、VBAと入力
+        self.GetWebElement(ElementType.Class, "cw-input_group_button", 10).find_element_by_tag_name(
+            "button").click()  # 検索ボタンを押す
 
-# get all of project's name
-def GetALlAnken(driver):
-    webElements = GetWebElements(driver, ElementType.Class, "item_title", 10)
-    return webElements
+    # get new projects'name
+    def GetNewProjectsInformation(self):
+        myDataOperation = DataOperation()           # create new instance of DataOperation
+        Select(self.GetWebElement(ElementType.Class, "cw-form_group", 10).find_element_by_tag_name(
+            "select")).select_by_index(1)           # select 新着
+        webElements = self.GetWebElements(ElementType.Class, "item_title", 10)
+        webNewElements = self.GetWebElements(ElementType.Class, "new", 10)
+        NewProjectsName = []
+        for i in range(len(webNewElements)):             # only append new projects
+            NewProjectsName.append(webElements[i].text)
+        NewProjectsID = myDataOperation.CreateAnkenID(NewProjectsName)                  # create ID
+        NewProjectsURL = []
+        for i in range(len(NewProjectsName)):                   # get new project's url
+            NewProjectsURL.append(webElements[i].find_element_by_tag_name("a").get_attribute("href"))
+        return NewProjectsID, NewProjectsName, NewProjectsURL
 
-# keyword search
-def SearchKeyword(driver, keyword):
-    GetWebElement(driver, ElementType.Class, "search_freeword", 10).find_element_by_tag_name("input").send_keys(keyword)      # 検索テキストボックスを取得、VBAと入力
-    GetWebElement(driver, ElementType.Class, "cw-input_group_button", 10).find_element_by_tag_name("button").click()  # 検索ボタンを押す
+    # all process from getting information
+    def GetAllInformation(self):
+        myDataOperation = DataOperation()           # create new instance of DataOperation
+        ALlAnkenName = self.GetALlAnkenName()  # get all of project's name
+        AllAnkenURL = self.GetAllAnkenURLs()  # get all of project's URL
+        ALlAnkenID = myDataOperation.CreateAnkenID(AllAnkenURL)  # create ID list for json
+        return ALlAnkenID, ALlAnkenName, AllAnkenURL
 
-# get new projects
-def GetNewProjects(driver):
-    Select(GetWebElement(driver, ElementType.Class, "cw-pull_left", 10).find_element_by_tag_name("select")).select_by_index(1)      # select 新着
-    webElement = GetWebElement(driver, ElementType.Class, "search_results", 10)
-    webElements = webElement.find_elements_by_class_name("new")
-    NewProjects = []
-    for eachElement in webElements:     # only append new projects
-        NewProjects.append(eachElement.text)
-    return NewProjects
+    # close chrome instance
+    def CloseChrome(self):
+        self.driver.close()
 
-# Convert data to JSON style
-def ConvertJSON(originalList):
-    return jsonify({'projects':originalList})
+# Class which operate data
+class DataOperation:
+    # Convert data to JSON style
+    def ConvertJSON(self, originalList):
+        return jsonify({'projects': originalList})
 
-# create a list for JSON
-def CreateDict(AllAnkenID, AllAnkenName, AllAnkenURL):
-    AllInfo = []
-    for i in range(len(AllAnkenID)):
-        AllInfo.append("{id: " + str(AllAnkenID[i]) + ", title: " + AllAnkenName[i] + ", url: " + AllAnkenURL[i] + "}")
-    return AllInfo
+    # create a list for JSON
+    def CreateDict(self, AllAnkenID, AllAnkenName, AllAnkenURL):
+        AllInfo = []
+        for i in range(len(AllAnkenID)):
+            AllInfo.append(
+                "{id: " + str(AllAnkenID[i]) + ", title: " + AllAnkenName[i] + ", url: " + AllAnkenURL[i] + "}")
+        return AllInfo
 
-# craete ID list for json
-def CreateAnkenID(AnkenList):
-    IDList = {}
-    for i in range(len(AnkenList)):
-        IDList[i] = i
-    return IDList
+    # craete ID list for json
+    def CreateAnkenID(self, AnkenList):
+        IDList = {}
+        for i in range(len(AnkenList)):
+            IDList[i] = i
+        return IDList
 
-# all process from getting information to convert that json
-def GetInfoAndCreateJSON(driver):
-    ALlAnkenName = GetALlAnkenName(driver)         # get all of project's name
-    AllAnkenURL = GetAllAnkenURLs(driver)          # get all of project's URL
-    ALlAnkenID = CreateAnkenID(AllAnkenURL)         # create ID list for json
-    driver.close()
-    infoDict = CreateDict(ALlAnkenID, ALlAnkenName, AllAnkenURL)            # create lists for JSON
-    jsonData = ConvertJSON(infoDict)            # convert to JSON style
-    return jsonData
